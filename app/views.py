40c197e092b0465171100996a7a6f70a1e640e6c
WatoDej <watsondejone5@gmail.com>
2026-03-16 22:33:05 -0500

    exercise 4 completed

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -20,7 +20,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Dejone Watson")
 
 
 @app.route('/upload', methods=['POST', 'GET'])
@@ -45,18 +45,23 @@ def login():
     # and not just one field
     if form.username.data:
         # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
+        user = UserProfile.query.filter_by(username=username).first()
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
+        if user and check_password_hash(user.password, password):
 
         # Gets user id, load into session
-        login_user(user)
+            login_user(user)
 
         # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+            flash('Login successful!', 'success')
+            return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
